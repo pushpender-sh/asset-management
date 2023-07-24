2aebd88838239c6d19257cb73e3eec7e87aba906
Pushpender Sharma <pushpendersharma2019@gmail.com>
2023-07-24 16:50:56 +0530

    login page completed

---
+++ src/Screens/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Login.css'
+import loginImage from '../Images/loginPageImage.jpg'
+export default function Login() {
+  return (
+    <div className='main-login-div'>
+     <div className='login-image'>
+        <img src={loginImage} alt="welcome user" style={{height: "600px", width:"590px"}} />
+     </div>
+    
+      <form className='login-details'>
+      <h2 style={{fontFamily:"Roboto"}}>Login to Admin Panel</h2>
+      <div className='imput'>
+      <label>Email </label> <br />
+      <input type="text" placeholder='Enter Email Id' style={{height: "40px", width:"300px"}}/>
+      </div>
+     <div className='imput'>
+      <label >Password</label> <br /> 
+      <input type="text" placeholder='Enter Password'  style={{height: "40px", width:"300px"}}/>
+      <div><input type="checkbox" /> Remember me </div>
+      </div>
+      <button className='login-button'>Log In</button>
+      </form>
+     </div>
+    
+  )
+}

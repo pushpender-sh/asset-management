e14604f812b43cc542e06c7956cd1e8396b1b73d
Pushpender Sharma <pushpendersharma2019@gmail.com>
2023-07-25 19:15:29 +0530

    cards done

--- src/Screens/DashBoard.jsx
+++ src/Screens/DashBoard.jsx
@@ -1,38 +1,58 @@
 import React, { useEffect } from 'react';
 import { useAuth } from './GlobalContext';
 import Navbar from '../Components/Navbar';
+import './DashBoard.css';
+import CircularProgress from '@mui/material/CircularProgress';
+import img1 from '../Images/img1.png'
+import img2 from '../Images/img2.png'
+import img3 from '../Images/img3.png'
+
+function Box({assetImage, assetName, assetCount, assetRaise}){
+    return(
+        <div className='box'>
+            <div className='imagediv'>
+            <img src={assetImage} alt="assetImage" style={{borderRadius:"100%", height:"50px"}} />
+            </div>
+             <div className='asset-details '>
+               <div style={{fontSize:"17px"}}> {assetName}</div>
+               <div style={{fontSize:"40px", fontWeight:"bold"}}> {assetCount} </div>
+                <div style={{fontSize:"13px"}}>{assetRaise}</div>
+             </div>
+        </div>
+    )
+}
+
+function BigCard(){
+    return(
+        <div className='card'>
+        card
+        </div>
+    )
+}
 
 export default function Dashboard() {
-  const { token } = useAuth();
+  const { token, assetvalue } = useAuth();
+  if(!assetvalue){
+    return(
+        <div> <CircularProgress/> <br />
+        Loading...</div>
+    )
+  }
+//   console.log(assetvalue.distributedAssets)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://devassetapi.remotestate.com/asset-management/user/dashboard', {
-          method: 'GET',
-          headers: {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-
-        const data = await response.json();
-        console.log(data.totalAssets);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, [token]);
-
+  
+  
   return (
     <div>
       <Navbar/>
+      <div className='asset-boxes'>
+      <Box assetImage={img1} assetName={"Total Assets"} assetCount={assetvalue.totalAssets} assetRaise={"7.8% this month"}/>
+      <Box assetImage={img2} assetName={"Distributed Assets"} assetCount={assetvalue.distributedAssets} assetRaise={"3.4% this month"}/>
+      <Box assetImage={img3} assetName={"Available Assets"} assetCount={assetvalue.availableAssets} />
+      </div>
+      <div className='bigcard'>
+      <BigCard/>
+      </div>
     </div>
   );
 }
--- src/Screens/GlobalContext.jsx
+++ src/Screens/GlobalContext.jsx
@@ -4,6 +4,7 @@ const AuthContext= createContext();
 
 export const AuthProvider=({children})=>{
     const[token, setToken]= useState('');
+    const[assetvalue, setAssetvalue]=useState();
 
 
     useEffect(() => {
@@ -15,8 +16,37 @@ export const AuthProvider=({children})=>{
       }, []);
 
 
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('https://devassetapi.remotestate.com/asset-management/user/dashboard', {
+              method: 'GET',
+              headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+              },
+            });
+    
+            if (!response.ok) {
+              throw new Error('Failed to fetch data');
+            }
+    
+            const data = await response.json();
+            setAssetvalue(data);
+            // console.log(data.totalAssets);
+            // console.log(assetvalue );
+            
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchData();
+      }, [token]);
+
+
     return(
-        <AuthContext.Provider value={{token, setToken}}>
+        <AuthContext.Provider value={{token, setToken, assetvalue, setAssetvalue}}>
             {children}
         </AuthContext.Provider>
     );

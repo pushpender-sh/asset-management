c28845ad344eca193c744bc9a6b42ea9815df621
Pushpender Sharma <pushpendersharma2019@gmail.com>
2023-07-26 23:58:32 +0530

    last

--- src/Screens/AssetLists.jsx
+++ src/Screens/AssetLists.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './AssetList.css'
+import { useAuth } from '../Screens/GlobalContext'
+
 import Navbar from './Navbar'
 import search from '../Images/search.jpg'
 import cross from '../Images/cross.png'
@@ -7,14 +9,16 @@ import cross from '../Images/cross.png'
 
 
 export default function AssetLists() {
+
+    const { assetDetails,  navigate} = useAuth();
+    // console.log(assetDetails);
+
   return (
-    <div className='top-div'>
-        <div className='navbar'>
+    <div className='top-div'>  
     <Navbar/>
-        </div>
         <div className='header-field'>
             <img src={search} alt="" />
-            <input type="text" placeholder='Search' />
+            <input type="text" placeholder='Search' style={{height: "30px"}}/>
             <img src={cross} alt="" />
 
         </div>
--- src/Screens/GlobalContext.jsx
+++ src/Screens/GlobalContext.jsx
@@ -7,6 +7,7 @@ const AuthContext= createContext();
 export const AuthProvider=({children})=>{
     const[token, setToken]= useState('');
     const[assetvalue, setAssetvalue]=useState();
+    const[assetDetails, setAssetDetails]=useState();
 
     const navigate = useNavigate('')
 
@@ -50,8 +51,39 @@ export const AuthProvider=({children})=>{
       }, [token]);
 
 
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('https://devassetapi.remotestate.com/asset-management/user/asset/', {
+              method: 'GET',
+              headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+              },
+            });
+    
+            if (!response.ok) {
+              throw new Error('Failed to fetch data');
+            }
+    
+            const data = await response.json();
+            setAssetDetails(data);
+            // console.log(data.totalAssets);
+            // console.log(assetvalue );
+            
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchData();
+      }, [token]);
+
+
+
+
     return(
-        <AuthContext.Provider value={{token, setToken, assetvalue, setAssetvalue, navigate}}>
+        <AuthContext.Provider value={{token, setToken, assetvalue, navigate, assetDetails,}}>
             {children}
         </AuthContext.Provider>
     );
